a10a14c3324237ba82889b144e013b21835e7545
evagvr <eva.gavriliu@gmail.com>
2026-06-29 17:59:05 +0300

    add KafkaProducerClient

--- ingestion/kafka_producer.py
+++ ingestion/kafka_producer.py
@@ -1,1 +1,23 @@
+import logging
+import json
 from kafka import KafkaProducer
+
+logger = logging.getLogger(__name__)
+
+class KafkaProducerClient:
+    def __init__(self, bootstrap_servers: str, topic: str):
+        self.producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers, 
+            value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+        self.topic = topic
+    
+    def send(self, message: str):
+        try:
+            self.producer.send(self.topic, message)
+            logger.info("Message was sent")
+        except Exception as e:
+            logger.error(e)
+            
+    def close(self):
+        self.producer.close()
+        logger.info("Kafka producer was closed")
